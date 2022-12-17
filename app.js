66cd5a5ae7146d2c83c9e6883881bc9673065b9e
arifvtti <arif.vtti@gmail.com>
2022-12-17 09:40:16 +0600

    make a simple route

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 
 
+app.get('/', (req, res) => {
+    res.send('<h1>I am simple server</h1>')
+})
+
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
