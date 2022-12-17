3aee068f41da89b24961e8f880e454f93b12f699
arifvtti <arif.vtti@gmail.com>
2022-12-17 09:49:25 +0600

    make multiple route and 404 route

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const express = require("express");
 const app = express();
 
 
+app.get('/about', (req, res) => {
+    res.send('<h1>This is a about page</h1>')
+})
+
+app.get('/help', (req, res) => {
+    res.send('<h1>This is a help page</h1>')
+})
+
 app.get('/', (req, res) => {
     res.send('<h1>I am simple server</h1>')
+})
+
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page Not Found</h1>')
 })
 
 
